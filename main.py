5f335599ebccfb693151934002d8f0d4923ccb59
Nurmuhammad11221 <roziyaabdurazoqova@gmail.com>
2024-05-08 13:21:46 +0500

    second commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# print 
+
+
+# print("hello world!")
+
+# print('g\'ofur')
+
+# print("g'ofur")
+
+# print('hello "bye" hello') 
+
+# print("""Lorem Ipsum is simply dummy text 
+# of the printing and typesetting industry.""")
+
+# print("Lorem Ipsum is simply dummy text \n of the printing and typesetting industry.")
+
+# print("Lorem Ipsum is simply dummy text \t of the printing and typesetting industry.")
+
+# arifmetik amallar
+
+# print(type(2 + 5))
+# print(type(5 / 2))
+# print(type(20/5))
+
+# print(2.5 + 2.5)
+
+# print(5 // 2)
+
+# print(5 % 2)
+
+# print(3 * 3)
+
+
+# print(3 ** 3)
+
+# print("3 ning 3 chi darajasi: ", 3 ** 3, "ga teng")
+
+# print("3 * 3 =", 3 * 3)
+
+# PI = 3.14
+# print(PI)
+
+aholi = 36_000_000
+print("uzb aholisi", aholi, "dan oshiq.")
